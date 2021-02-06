ad55296be6ecafc611cca47e7893aa4f2a1e4d12
bcherrera14 <bcherrera14@gmail.com>
2021-02-05 19:21:44 -0800

    initial commit

---
+++ BattleshipApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BattleshipApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string appName = "Battleship Console App";
+            string author = "Bryan Herrera";
+            //Change text color
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            //App title
+            Console.WriteLine("{0} by {1}", appName, author);
+            //Reset text color
+            Console.ResetColor();
+        }
+    }
+}
